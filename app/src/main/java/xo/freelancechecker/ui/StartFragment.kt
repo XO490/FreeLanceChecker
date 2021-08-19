8053cfb4522d461b9c45abe955a591f074d8975d
XO490 <xo490@protonmail.com>
2021-08-19 21:35:04 +0700

    finished for today: mipmap

--- app/src/main/java/xo/freelancechecker/ui/StartFragment.kt
+++ app/src/main/java/xo/freelancechecker/ui/StartFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentContainer
+import xo.freelancechecker.MainActivity
 import xo.freelancechecker.R
 import xo.freelancechecker.databinding.FragmentStartBinding
+import xo.freelancechecker.utils.APP_ACTIVITY
 
 class StartFragment : Fragment() {
 
@@ -20,5 +22,13 @@ class StartFragment : Fragment() {
     ):View? {
         _binding = FragmentStartBinding.inflate(layoutInflater, container, false)
         return mBinding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mBinding.buttonSearch.setOnClickListener {
+            APP_ACTIVITY.mNavController.navigate(R.id.action_startFragment_to_mainFragment)
+            APP_ACTIVITY.title = "Список задач"
+        }
     }
 }
--- readme.md
+++ readme.md
@@ -1,8 +1,22 @@
 # [Android App ┆ FreeLanceChecker](https://github.com/XO490/FreeLanceChecker.git)
 
+### Find your task that you want
+## on [freelance.habr.com](https://freelance.habr.com)
+
+----
+
+## Welcome to DEV branch
+
+# TODO:
+- [ ] UI
+    - [x] Start Fragment
+    - [ ] Main Fragment 
+    - [ ] Details Fragment
+    
+- [ ] BACK 
+    - [ ] Model and pars data
+    - [ ] LiveData
+    - [ ] endless scroll
 
 
-### Warning
-
-Master is empty, switch to the dev branch
  
