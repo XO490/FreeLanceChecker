1faf3da508e272d5b7647d6570ce0a1ea0cfb43f
XO490 <xo490@protonmail.com>
2021-08-19 15:54:01 +0700

    custom toolbar. initialization fragment on mainActivity

--- app/src/main/java/xo/freelancechecker/MainActivity.kt
+++ app/src/main/java/xo/freelancechecker/MainActivity.kt
@@ -2,10 +2,32 @@ package xo.freelancechecker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import xo.freelancechecker.databinding.ActivityMainBinding
+import xo.freelancechecker.utils.APP_ACTIVITY
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mToolbar: Toolbar
+    lateinit var mNavController: NavController
+    private var _bindind: ActivityMainBinding? = null
+    private val mBinding get() = _bindind!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _bindind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        APP_ACTIVITY = this
+        mToolbar = mBinding.toolbar
+        mNavController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        setSupportActionBar(mToolbar)
+        title = getString(R.string.toolbar_title)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _bindind = null
     }
 }
---
+++ app/src/main/java/xo/freelancechecker/utils/constants.kt
@@ -0,0 +1,5 @@
+package xo.freelancechecker.utils
+
+import xo.freelancechecker.MainActivity
+
+lateinit var APP_ACTIVITY: MainActivity
